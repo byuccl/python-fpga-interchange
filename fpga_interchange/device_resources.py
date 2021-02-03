b36ca713b4d25f86ef205c6da68ce7d1cf53efa5
ReillyMcK <romckend@byu.edu>
2021-02-02 17:43:19 -0700

    XDLRC in now a class that extends DeviceResources

--- fpga_interchange/device_resources.py
+++ fpga_interchange/device_resources.py
@@ -642,8 +642,6 @@ class DeviceResources():
 
         self.tile_name_to_tile = {}
         self.site_name_to_site = {}
-        self.tiles = []
-        tiles_by_row = [[]]
         for tile_idx, tile in enumerate(self.device_resource_capnp.tileList):
             tile_name = self.strs[tile.name]
             tile_name_index = self.string_index[tile_name]
@@ -660,14 +658,6 @@ class DeviceResources():
                 tile_type_index=tile_type_index,
                 tile_type=self.strs[tile_type.name],
                 site_names=site_names)
-
-            # Create a list of lists of tiles by row
-            if len(tiles_by_row) <= tile.row:
-                for i in range(tile.row - len(tiles_by_row)):
-                    tiles_by_row.append([])
-                tiles_by_row.append([tile])
-            else:
-                tiles_by_row[tile.row].append(tile)
 
             for site_idx, site in enumerate(tile.sites):
                 site_name = self.strs[site.name]
@@ -702,25 +692,9 @@ class DeviceResources():
                         alt_index=alt_index,
                         site_type_name=site_type_name)
 
-        # sort each row list by column and then attach to master tile list
-        for tile_row in tiles_by_row:
-            tile_row.sort(key=DeviceResources.__sort_tile_cols__)
-            self.tiles += tile_row
-
         self.tile_wire_index_to_node_index = None
         self.parameter_definitions = None
         self.parameters_for_cell = None
-
-    def __sort_tile_cols__(tile):
-        """
-        Helper function for sort.
-
-        NOT designed for use outside of being a key function for sort().
-        Helps sort() sort the tiles based on col number
-
-        NOTE: self is purposely not included as the first arguement.
-        """
-        return tile.col
 
     def build_node_index(self):
         """ Build node index for looking up wires to nodes. """
@@ -892,7 +866,9 @@ class DeviceResources():
             pin_name=pin_name,
             wire_name=wire_name)
 
-    def get_constraints(self):
+
+<< << << < HEAD
+   def get_constraints(self):
         constraints = Constraints()
         constraints.read_constraints(self.device_resource_capnp.constraints)
 
@@ -920,7 +896,65 @@ class DeviceResources():
 
     def get_constants(self):
         constants = self.device_resource_capnp.constants
+== == == =
 
+
+class XDLRC(DeviceResources):
+    """
+    Class for generating XDLRC files from Interchange device resources.
+
+    This class contains the main/helper routines associated with
+    generating a XDLRC file.  Creating an instance of the class
+    automatically will generate the XDLRC file.
+
+    Constructor Parameters:
+    device_rep (DeviceResources)
+    file_name (String) - filename for xdlrc file (.xdlrc extension will
+                         be appended).
+                         Default: device_resource_capnp.name
+    """
+
+    def __sort_tile_cols__(tile):
+        """
+        Helper function for sort.
+
+        NOT designed for use outside of being a key function for sort().
+        Helps sort() sort the tiles based on col number
+
+        NOTE: self is purposely not included as the first arguement.
+        """
+        return tile.col
+
+    def __init__(self, device_resource_capnp, fileName=''):
+        super().__init__(device_resource_capnp)
+
+        self.tiles = []
+        tiles_by_row = [[]]
+        for tile in self.device_resource_capnp.tileList:
+            # Create a list of lists of tiles by row
+            if len(tiles_by_row) <= tile.row:
+                for i in range(tile.row - len(tiles_by_row)):
+                    tiles_by_row.append([])
+                tiles_by_row.append([tile])
+            else:
+                tiles_by_row[tile.row].append(tile)
+
+        # sort each row list by column and then attach to master tile list
+        for tile_row in tiles_by_row:
+            tile_row.sort(key=XDLRC.__sort_tile_cols__)
+            self.tiles += tile_row
+
+        self.generate_XDLRC(fileName)
+
+    Direction_strs = ("Input", "Output", "Inout")
+
+    def Direction_to_str(dir):
+        """.logical_netlist.Direction to XDLRC direction string"""
+        return
+
+    def get_constants(self):
+        constants = self.device_resource_capnp.constants
+        
         # Have a default in case it can be any name!
         vcc_net_name = 'GLOBAL_LOGIC1'
         gnd_net_name = 'GLOBAL_LOGIC0'
@@ -987,8 +1021,8 @@ class DeviceResources():
         """ Return ParameterDefinition class if parameter exists, else None.
 
         Arguments:
-            cell_type (str) - Cell type to get parameter definition for.
-            parameter_name (str) - Parameter name to get definition.
+            cell_type(str) - Cell type to get parameter definition for.
+            parameter_name(str) - Parameter name to get definition.
 
         Returns:
             Either returns ParameterDefinition class if the parameter exists
@@ -1010,9 +1044,8 @@ class DeviceResources():
         value.
 
         Arguments:
-            cell_type (str) - Cell type to get default parameters for.
-            property_map (dict-like of str to str) - Property map for cell.
-
+            cell_type(str) - Cell type to get default parameters for.
+            property_map(dict-like of str to str) - Property map for cell.
 
         """
         if self.parameter_definitions is None:
