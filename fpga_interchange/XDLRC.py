7d6de3d3c2f83637254aaead551a83329cbbf3e2
ReillyMcK <romckend@byu.edu>
2021-02-11 02:15:23 -0700

    Bug fix in getting all conns in an element

--- fpga_interchange/XDLRC.py
+++ fpga_interchange/XDLRC.py
@@ -36,7 +36,7 @@ class XDLRC(DeviceResources):
 
         self.tiles = []
         tiles_by_row = [[]]
-        for tile in raw_repr.tileList:
+        for tile in self.device_resource_capnp.tileList:
             # Create a list of lists of tiles by row
             if len(tiles_by_row) <= tile.row:
                 for i in range(tile.row - len(tiles_by_row)):
@@ -79,26 +79,24 @@ class XDLRC(DeviceResources):
         # TILE declaration
         for tile in self.tiles:
             tile_name = self.strs[tile.name]
-            if (tile_name == 'LIOB33_SING_X0Y199'):
 
-                tile_type = self.get_tile_type(tile.type)
-                wires = tile_type.wires
-                pips = tile_type.pips
-                xdlrc.write(f"\t(tile {tile.row} {tile.col} {tile_name} "
-                            + f"{tile_type.name} {len(tile.sites)}\n")
+            tile_type = self.get_tile_type(tile.type)
+            wires = tile_type.wires
+            pips = tile_type.pips
+            xdlrc.write(f"\t(tile {tile.row} {tile.col} {tile_name} "
+                        + f"{tile_type.name} {len(tile.sites)}\n")
 
-                num_wires = len(wires)
-                num_pips = len(pips)
-                num_primitive_sites = len(tile.sites)
+            num_wires = len(wires)
+            num_pips = len(pips)
+            num_primitive_sites = len(tile.sites)
 
-                # PRIMITIVE_SITE declaration
-                for site in tile.sites:
-                    site_name = self.strs[site.name]
-                    site_t_info = self.site_name_to_site[
-                        site_name]
-                    assert len(list(site_t_info)) < 2
-                    site_t_name = list(site_t_info)[0]
-                    site = site_t_info[site_t_name]
+            # PRIMITIVE_SITE declaration
+            for site in tile.sites:
+                site_name = self.strs[site.name]
+                site_t_infos = self.site_name_to_site[
+                    site_name]
+
+                for site_t_name, site in site_t_infos.items():
                     site_t = self.get_site_type(site.site_type_index)
                     xdlrc.write(f"\t\t(primitive_site {site_name} "
                                 + f"{site_t_name} "
@@ -106,7 +104,13 @@ class XDLRC(DeviceResources):
 
                     # PINWIRE declaration
                     for idx, pin in enumerate(site_t.site_pins.items()):
-                        pin_wire = self.get_site_pin(site, idx).wire_name
+                        try:
+                            pin_wire = self.get_site_pin(site, idx).wire_name
+                        except IndexError as e:
+                            # TODO investigate
+                            if 'BRAM' not in tile_name:
+                                print("Error pin idx out of bounds.")
+                                print(f"tile_name {tile_name} Site {site}")
                         pin_name = pin[0]  # key value is pin_name
                         pin = pin[1]  # value is pin data
                         direction = pin[3].name.lower()
@@ -114,57 +118,65 @@ class XDLRC(DeviceResources):
                                     + f"{direction} {pin_wire})\n")
                     xdlrc.write(f"\t\t)\n")
 
-                # WIRE declaration
-                for idx in tile_type.string_index_to_wire_id_in_tile_type.keys():  # noqa
-                    wire_name = self.strs[idx]
-                    try:
-                        node_idx = self.node(tile_name, wire_name).node_index
-                    except AssertionError as e:
-                        num_wires -= 1
-                        continue
-                    myNode = raw_repr.nodes[node_idx]
-                    xdlrc.write(
-                        f"\t\t(wire {wire_name} {len(myNode.wires) -1}\n")
+            # WIRE declaration
+            for idx in tile_type.string_index_to_wire_id_in_tile_type.keys():  # noqa
+                wire_name = self.strs[idx]
+                try:
+                    node_idx = self.node(tile_name, wire_name).node_index
+                except AssertionError as e:
+                    num_wires -= 1
+                    continue
+                myNode = raw_repr.nodes[node_idx]
+                xdlrc.write(
+                    f"\t\t(wire {wire_name} {len(myNode.wires) -1}\n")
 
-                    # CONN declaration
-                    for w in myNode.wires:
-                        wire = raw_repr.wires[w]
-                        conn_tile = self.strs[wire.tile]
-                        conn_wire = self.strs[wire.wire]
+                # CONN declaration
+                for w in myNode.wires:
+                    wire = raw_repr.wires[w]
+                    conn_tile = self.strs[wire.tile]
+                    conn_wire = self.strs[wire.wire]
 
-                        if conn_wire != wire_name:
-                            xdlrc.write(
-                                f"\t\t\t(conn {conn_tile} {conn_wire})\n")
+                    if conn_wire != wire_name:
+                        xdlrc.write(
+                            f"\t\t\t(conn {conn_tile} {conn_wire})\n")
 
-                    xdlrc.write(f"\t\t)\n")
+                xdlrc.write(f"\t\t)\n")
 
-                # PIP declaration
-                for p in pips:
-                    xdlrc.write(
-                        f"\t\t(pip {tile_name} {self.strs[wires[p.wire0]]} ->"
-                        + f" {self.strs[wires[p.wire1]]})\n")
+            # PIP declaration
+            for p in pips:
+                xdlrc.write(
+                    f"\t\t(pip {tile_name} {self.strs[wires[p.wire0]]} ->"
+                    + f" {self.strs[wires[p.wire1]]})\n")
 
-                # TILE_SUMMARY declaration
-                xdlrc.write(f"\t\t(tile_summary {tile_name} {tile_type.name} ")
-                xdlrc.write(f"{num_primitive_sites} {num_wires} {num_pips})\n")
-                xdlrc.write(f"\t)\n")
-                return
+            # TILE_SUMMARY declaration
+            xdlrc.write(f"\t\t(tile_summary {tile_name} {tile_type.name} ")
+            xdlrc.write(f"{num_primitive_sites} {num_wires} {num_pips})\n")
+            xdlrc.write(f"\t)\n")
 
         # PRIMITIVE_DEFS declaration
         # TODO calculate total # primitive defs
         xdlrc.write(f")\n (primitive_defs {''}\n")
 
         # PRIMITIVE_DEF declaration
+        # Semantics to ensure primitive_defs are added alphabetically
+        site_types = {}
         for idx in range(len(raw_repr.siteTypeList)):
             site_t = self.get_site_type(idx)
+            site_types[site_t.site_type] = site_t
+
+        site_type_names = list(site_types.keys()).sort()
+
+        for i in site_type_names:
+            site_t = site_types[i]
             site_t_raw = raw_repr.siteTypeList[site_t.site_type_index]
             site_wires = site_t_raw.siteWires
 
-            # PINWIRE declaration
+            # PIN declaration
             for pin_name, pin in site_t.site_pins.items():
                 direction = pin[3].name.lower()
+                # TODO make sure pin_name == wire_name
                 xdlrc.write(
-                    f"\t\t(pinwire {pin_name} {pin_name} {direction})\n")
+                    f"\t\t(pin {pin_name} {pin_name} {direction})\n")
 
             # ELEMENT declaration
             for bel in site_t.bels:
@@ -180,24 +192,19 @@ class XDLRC(DeviceResources):
 
                     # CONN declaration
                     site_wire_index = bel_info[1]
-
-                    # TODO This fails ex ST: DSP48E1 SW: ALUMODE0 # noqa
-                    assert len(site_wires[site_wire_index].pins) == 2
-
                     for bel_pin2 in site_wires[site_wire_index].pins:
                         if bel_pin2[0] != bel.name:
-                            break
+                            bel2_name = bel_pin2[0]
+                            bel_pin2_name = bel_pin2[1]
 
-                    bel2_name = bel_pin2[0]
-                    bel_pin2_name = bel_pin2[1]
+                            direction = bel_info[2]
+                            direction_str = ''
+                            if direction == Direction.Input:
+                                direction_str = '<=='
+                            elif direction == Direction.Output:
+                                direction_str = '==>'
 
-                    direction = bel_info[2]
-                    direction_str = ''
-                    if direction == Direction.Input:
-                        direction_str = '<=='
-                    elif direction == Direction.Output:
-                        direction_str = '==>'
-
-                    xdlrc.write(f"\t\t\t(conn {bel.name} {bel_pin_name} "
-                                + f"{direction_str} {bel2_name} "
-                                + f"{bel_pin2_name})\n")
+                            xdlrc.write(f"\t\t\t(conn {bel.name} "
+                                        + f"{bel_pin_name} "
+                                        + f"{direction_str} {bel2_name}"
+                                        + f" {bel_pin2_name})\n")
