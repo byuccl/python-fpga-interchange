fef2bd087ae8cbfad247bdb09831f9e3dba76ed5
ReillyMcK <romckend@byu.edu>
2021-03-05 12:13:47 -0700

    Split functions for easier partial XDLRC generation

--- fpga_interchange/XDLRC.py
+++ fpga_interchange/XDLRC.py
@@ -29,6 +29,15 @@ class XDLRC(DeviceResources):
         return tile.col
 
     def __init__(self, device_resource, fileName=''):
+        """
+        Initialize the XDLRC object.
+
+        Parameters:
+            device_resource - Object to obtain device information from.
+                              Can be instance of DeviceResources or
+                              interchange_capnp.read_capnp_file() output
+            fileName (str)  - Name of file to create/write to
+        """
         if type(device_resource) is DeviceResources:
             # TODO test this feature
             self.__dict__ = device_resource.__dict__.copy()
@@ -51,119 +60,126 @@ class XDLRC(DeviceResources):
             tile_row.sort(key=XDLRC.__sort_tile_cols__)
             self.tiles += tile_row
 
-        self.generate_XDLRC(fileName)
-
-    def generate_XDLRC(self, fileName=''):
-        """
-        UNDER CONSTRUCTION
-        Generate an XDLRC file based on the DeviceResources Device.
-
-        fileName (String) - filename for xdlrc file (.xdlrc extension
-            will be appended). Default: self.device_resource_capnp.name
-        """
-
-        # "Pointer" to r capnp device resource data (reduce typing)
-        raw_repr = self.device_resource_capnp
-
+        # set up file to write to
         if fileName == '':
-            fileName = raw_repr.name
+            fileName = self.device_resource_capnp.name + ".xdlrc"
+        self.xdlrc = open(fileName, "w+")
 
-        fileName = fileName + '.xdlrc'
+    def close_file(self):
+        self.xdlrc.close()
 
-        xdlrc = open(fileName, "w+")
+    def _generate_tile(self, tile):
+        """The heavy lifting for generating xdlrc for a tile."""
 
-        # TILES declaration
-        num_rows = self.tiles[-1].row + 1
-        num_cols = self.tiles[-1].col + 1
-        xdlrc.write(f"(tiles {num_rows} {num_cols}\n")
+        # Some pointers for abbreviated reference
+        raw_repr = self.device_resource_capnp
+        xdlrc = self.xdlrc
 
-        # TILE declaration
+        tile_name = self.strs[tile.name]
+
+        tile_type = self.get_tile_type(tile.type)
+        tile_type_r = raw_repr.tileTypeList[tile_type.tile_type_index]
+        wires = tile_type.wires
+        pips = tile_type.pips
+        xdlrc.write(f"\t(tile {tile.row} {tile.col} {tile_name} "
+                    + f"{tile_type.name} {len(tile.sites)}\n")
+
+        num_wires = 0
+        num_pips = len(pips)
+        num_pinwires = 0
+
+        pin_tile_wires = set()
+
+        # PRIMITIVE_SITE declaration
+        for site in tile.sites:
+            site_name = self.strs[site.name]
+            site_type_in_tile_type = tile_type_r.siteTypes[site.type]
+            site_type_r_idx = site_type_in_tile_type.primaryType
+            site_type_r = raw_repr.siteTypeList[site_type_r_idx]
+            site_t_name = self.strs[site_type_r.name]
+            site = self.site_name_to_site[site_name][site_t_name]
+
+            site_t = self.get_site_type(site.site_type_index)
+            xdlrc.write(f"\t\t(primitive_site {site_name} {site_t_name} "
+                        + f"{len(site_t.site_pins.keys())}\n")
+
+            # PINWIRE declaration
+            # site_pin to tile_wire list
+            site_to_tile = site_type_in_tile_type.primaryPinsToTileWires
+            for idx, pin in enumerate(site_type_r.pins):
+                pin_name = self.strs[pin.name]
+                tile_wire = self.strs[site_to_tile[idx]]
+                pin = site_t.site_pins[pin_name]
+                direction = pin[3].name.lower()
+                num_pinwires += 1
+                pin_tile_wires.add(tile_wire)
+                xdlrc.write(f"\t\t\t(pinwire {pin_name} {direction} "
+                            + f"{tile_wire})\n")
+            xdlrc.write(f"\t\t)\n")
+
+        # WIRE declaration
+        tile_wires = set()
+        for idx in tile_type.string_index_to_wire_id_in_tile_type.keys():
+            wire_name = self.strs[idx]
+            try:
+                node_idx = self.node(tile_name, wire_name).node_index
+            except AssertionError as e:
+                continue
+            myNode = raw_repr.nodes[node_idx]
+
+            num_wires += 1
+            tile_wires.add(wire_name)
+            xdlrc.write(f"\t\t(wire {wire_name} {len(myNode.wires) -1}")
+
+            if len(myNode.wires) == 1:  # no CONNs
+                xdlrc.write(')\n')
+                continue
+            else:
+                xdlrc.write('\n')
+
+            # CONN declaration
+            for w in myNode.wires:
+                wire = raw_repr.wires[w]
+                conn_tile = self.strs[wire.tile]
+                conn_wire = self.strs[wire.wire]
+
+                if conn_wire != wire_name:
+                    xdlrc.write(f"\t\t\t(conn {conn_tile} {conn_wire})\n")
+
+            xdlrc.write(f"\t\t)\n")
+
+        for wire in (pin_tile_wires - tile_wires):
+            num_wires += 1
+            xdlrc.write(f"\t\t(wire {wire} {0})\n")
+
+        # PIP declaration
+        for p in pips:
+            xdlrc.write(f"\t\t(pip {tile_name} {self.strs[wires[p.wire0]]} ->"
+                        + f" {self.strs[wires[p.wire1]]})\n")
+
+        # TILE_SUMMARY declaration
+        xdlrc.write(f"\t\t(tile_summary {tile_name} {tile_type.name} ")
+        xdlrc.write(f"{num_pinwires} {num_wires} {num_pips})\n")
+        xdlrc.write(f"\t)\n")
+
+    def generate_tile(self, tile_name):
+        """Generate a single tile representation for tile_name (str)."""
         for tile in self.tiles:
-            tile_name = self.strs[tile.name]
+            name = self.strs[tile.name]
+            if name == tile_name:
+                self._generate_tile(tile)
 
-            tile_type = self.get_tile_type(tile.type)
-            tile_type_r = raw_repr.tileTypeList[tile_type.tile_type_index]
-            wires = tile_type.wires
-            pips = tile_type.pips
-            xdlrc.write(f"\t(tile {tile.row} {tile.col} {tile_name} "
-                        + f"{tile_type.name} {len(tile.sites)}\n")
+    def generate_prim_defs(self):
+        """Generate the primitive_defs."""
 
-            num_wires = 0
-            num_pips = len(pips)
-            num_pinwires = 0
-
-            # PRIMITIVE_SITE declaration
-            for site in tile.sites:
-                site_name = self.strs[site.name]
-                site_type_in_tile_type = tile_type_r.siteTypes[site.type]
-                site_type_r_idx = site_type_in_tile_type.primaryType
-                site_type_r = raw_repr.siteTypeList[site_type_r_idx]
-                site_t_name = self.strs[site_type_r.name]
-                site = self.site_name_to_site[site_name][site_t_name]
-
-                site_t = self.get_site_type(site.site_type_index)
-                xdlrc.write(f"\t\t(primitive_site {site_name} "
-                            + f"{site_t_name} "
-                            + f"{len(site_t.site_pins.keys())}\n")
-
-                # PINWIRE declaration
-                # site_pin to tile_wire list
-                site_to_tile = site_type_in_tile_type.primaryPinsToTileWires
-                for idx, pin in enumerate(site_type_r.pins):
-                    pin_name = self.strs[pin.name]
-                    tile_wire = self.strs[site_to_tile[idx]]
-                    pin = site_t.site_pins[pin_name]
-                    direction = pin[3].name.lower()
-                    num_pinwires += 1
-                    xdlrc.write(f"\t\t\t(pinwire {pin_name} "
-                                + f"{direction} {tile_wire})\n")
-                xdlrc.write(f"\t\t)\n")
-
-            # WIRE declaration
-            for idx in tile_type.string_index_to_wire_id_in_tile_type.keys():
-                wire_name = self.strs[idx]
-                try:
-                    node_idx = self.node(tile_name, wire_name).node_index
-                except AssertionError as e:
-                    continue
-                myNode = raw_repr.nodes[node_idx]
-
-                num_wires += 1
-                xdlrc.write(
-                    f"\t\t(wire {wire_name} {len(myNode.wires) -1}")
-                if len(myNode.wires) == 1:  # no CONNs
-                    xdlrc.write(')\n')
-                    continue
-                else:
-                    xdlrc.write('\n')
-
-                # CONN declaration
-                for w in myNode.wires:
-                    wire = raw_repr.wires[w]
-                    conn_tile = self.strs[wire.tile]
-                    conn_wire = self.strs[wire.wire]
-
-                    if conn_wire != wire_name:
-                        xdlrc.write(
-                            f"\t\t\t(conn {conn_tile} {conn_wire})\n")
-
-                xdlrc.write(f"\t\t)\n")
-
-            # PIP declaration
-            for p in pips:
-                xdlrc.write(
-                    f"\t\t(pip {tile_name} {self.strs[wires[p.wire0]]} ->"
-                    + f" {self.strs[wires[p.wire1]]})\n")
-
-            # TILE_SUMMARY declaration
-            xdlrc.write(f"\t\t(tile_summary {tile_name} {tile_type.name} ")
-            xdlrc.write(f"{num_pinwires} {num_wires} {num_pips})\n")
-            xdlrc.write(f"\t)\n")
+        # some pointers for abbreviated reference
+        raw_repr = self.device_resource_capnp
+        xdlrc = self.xdlrc
 
         # PRIMITIVE_DEFS declaration
         xdlrc.write(f")\n (primitive_defs {len(raw_repr.siteTypeList)}\n")
 
-        # PRIMITIVE_DEF declaration
+        # PRIMITIVE_DEF declarations
         # Semantics to ensure primitive_defs are added alphabetically
         site_types = {}
         for idx in range(len(raw_repr.siteTypeList)):
@@ -221,3 +237,24 @@ class XDLRC(DeviceResources):
                                         + f"{bel_pin_name} "
                                         + f"{direction_str} {bel2_name}"
                                         + f" {bel_pin2_name})\n")
+
+    def generate_XDLRC(self):
+        """
+        UNDER CONSTRUCTION
+        Generate an XDLRC file based on the DeviceResources Device.
+        """
+
+        # TILES declaration
+        num_rows = self.tiles[-1].row + 1
+        num_cols = self.tiles[-1].col + 1
+        self.xdlrc.write(f"(tiles {num_rows} {num_cols}\n")
+
+        # TILE declarations
+        for tile in self.tiles:
+            self._generate_tile(tile)
+
+        # PRIMITIVE_DEFS
+        self.generate_prim_defs()
+
+        # cleanup
+        self.close_file()
